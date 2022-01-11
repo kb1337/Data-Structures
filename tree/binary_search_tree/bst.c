1d3a525c6f602061a31f8bce44d95b75ba506f7b
Kadir Burak Gündüz <kburakgndz@gmail.com>
2022-01-12 00:19:10 +0300

    binary search tree | source and header files added

---
+++ tree/binary_search_tree/bst.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "bst.h"
+
+void *AllocateMemory(int sizeBytes, const char *strErr, bool bExit)
+{
+    void *pvMem = malloc(sizeBytes);
+
+    if (NULL == pvMem)
+    {
+        fprintf(stderr, "Memory allocation failure: %s\n", strErr);
+
+        if (bExit)
+            exit(EXIT_FAILURE);
+    }
+
+    return pvMem;
+}
+
+NODE *AddNode(NODE *tree, int data)
+{
+    if (NULL == tree)
+    {
+        NODE *newNode = AllocateMemory(sizeof(NODE), "AddNode", EXIT_PROGRAM);
+        newNode->data = data;
+        newNode->left = NULL;
+        newNode->right = NULL;
+
+        return newNode;
+    }
+    else
+    {
+        if (tree->data < data)
+        {
+            tree->right = AddNode(tree->right, data);
+        }
+        else
+        {
+            tree->left = AddNode(tree->left, data);
+        }
+        return tree;
+    }
+}
+
+void PrintTree(NODE *root)
+{
+    if (NULL == root)
+        return;
+
+    PrintTree(root->left);
+    printf("Data: %d\n", root->data);
+    PrintTree(root->right);
+}
+
+void ReverseTree(NODE *tree)
+{
+    if (NULL == tree)
+        return;
+
+    NODE *tmp = tree->right;
+    tree->right = tree->left;
+    tree->left = tmp;
+
+    ReverseTree(tree->left);
+    ReverseTree(tree->right);
+}
+
+NODE *FindNode(NODE *tree, int data)
+{
+    if (NULL == tree || tree->data == data)
+        return tree;
+
+    if (tree->data < data)
+        return FindNode(tree->right, data);
+
+    return FindNode(tree->left, data);
+}
+
+int FindMaxValue(NODE *tree)
+{
+
+    while (tree->right != NULL)
+        tree = tree->right;
+
+    return tree->data;
+}
+
+void DestroyTree(NODE *tree)
+{
+    if (NULL == tree)
+        return;
+
+    DestroyTree(tree->left);
+    DestroyTree(tree->right);
+    free(tree);
+}
---
+++ tree/binary_search_tree/bst.h
@@ -0,0 +1,29 @@
+#ifndef bst_h
+#define bst_h
+
+#include <stdbool.h>
+
+#define EXIT_PROGRAM true
+#define DO_NOT_EXIT_PROGRAM false
+
+typedef struct NODE
+{
+    int data;
+    struct NODE *left;
+    struct NODE *right;
+} NODE;
+
+void *AllocateMemory(int sizeBytes, const char *strErr, bool bExit);
+
+NODE *AddNode(NODE *tree, int data);
+
+void PrintTree(NODE *tree);
+
+void ReverseTree(NODE *tree);
+
+NODE *FindNode(NODE *tree, int data);
+int FindMaxValue(NODE *tree);
+
+void DestroyTree(NODE *tree);
+
+#endif
