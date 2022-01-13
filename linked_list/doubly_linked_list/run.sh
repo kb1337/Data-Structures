5bfc480c8e6577edcfe029f266d89f4a1f813a85
Kadir Burak Gündüz <kburakgndz@gmail.com>
2022-01-13 23:15:28 +0300

    doubly linked list | run script added

---
+++ linked_list/doubly_linked_list/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Build program and run
+
+set -e
+# -e Exit immediately if a command exits with a non-zero status
+
+CC=cc
+CFLAGS="-Wall -Wextra"
+
+$CC $CFLAGS -o doubly_linked_list main.c dll.c
+
+./doubly_linked_list
